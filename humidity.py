f348409839966463f4fc22e66ce2711ad8c1acf3
noahstammm <95745547+noahstammm@users.noreply.github.com>
2024-01-06 10:59:02 +0100

    Update humidity.py

--- humidity.py
+++ humidity.py
@@ -1,27 +1,25 @@
-Python
-#!/usr/bin/python
 import spidev
-import os
 import time
- 
-delay = 0.2
- 
+
+# Konfiguration des MCP3008 ADC
 spi = spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz=1000000
- 
-def readChannel(channel):
-  val = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((val[1]&3) << 8) + val[2]
-  return data
- 
-if __name__ == "__main__":
-  try:
+spi.open(0, 0)  # Bus 0, Gerät 0
+spi.max_speed_hz = 1000000  # 1 MHz (kann je nach Sensor variieren)
+
+def read_adc(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+# Anpassen Sie den Kanal entsprechend Ihrer Verkabelung
+moisture_channel = 0
+
+try:
     while True:
-      val = readChannel(0)
-      if (val != 0):
-        print(val)
-      time.sleep(delay)
-      
-  except KeyboardInterrupt:
-    print "Cancel."
+        moisture_level = read_adc(moisture_channel)
+        print('Moisture Level:', moisture_level)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    spi.close()
+
