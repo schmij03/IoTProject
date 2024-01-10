717f81ba02a06690ab29a3e82f1637d6ea353978
schmij03 <96172389+schmij03@users.noreply.github.com>
2024-01-10 17:51:12 +0100

    Delete sensor.py

--- sensor.py
+++
@@ -1,9 +0,0 @@
-import fake_rpi as GPIO
-
-MOISTURE_SENSOR_PIN = 17  # Change to your GPIO pin
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOISTURE_SENSOR_PIN, GPIO.IN)
-
-def read_soil_moisture():
-    return GPIO.input(MOISTURE_SENSOR_PIN)
